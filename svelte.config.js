5985a31562eebbddbef18f8966a20749fbca3ef7
ZackBudai <zwatermelon@proton.me>
2025-04-06 19:26:36 +1000

    fixx

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/pages' : ''
+			base: '/pages'
 		},
 		prerender: {
 			handleHttpError: 'warn',
