4d2703cfff809723e8e1c951bed39dd4668fd712
ZackBudai <zwatermelon@proton.me>
2025-04-25 12:45:05 +0000

    testing url changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Projects } from './pages/Projects';
-import { ProjectDetails } from './pages/ProjectDetails';
-import About from './pages/About';
-import Academics from './pages/Academics';
+import { Outlet } from 'react-router-dom';
 import { TransitionLayout } from './components/TransitionLayout';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <TransitionLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDetails />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/academics" element={<Academics />} />
-        </Routes>
-      </TransitionLayout>
-    </Router>
+    <TransitionLayout>
+      <Outlet />
+    </TransitionLayout>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import './index.css'
+import { Home } from './pages/Home'
+import { Projects } from './pages/Projects'
+import { ProjectDetails } from './pages/ProjectDetails'
+import About from './pages/About'
+import Academics from './pages/Academics'
 import App from './App.tsx'
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "projects", element: <Projects /> },
+      { path: "project/:id", element: <ProjectDetails /> },
+      { path: "about", element: <About /> },
+      { path: "academics", element: <Academics /> }
+    ]
+  }
+], {
+  // This will make the hash router use nicer URLs
+  future: {
+    v7_prependBasename: true
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
